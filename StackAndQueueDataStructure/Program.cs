aaf7976e1600516444f50a33efa90bf7780f49e9
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-30 21:21:15 +0530

    implemented the usecase 1 to create a Stack of 56->30->70 - Use LinkedList to do the Stack Operations - Here push will internally call add method on LinkedList. - So 70 will be added first then 30 and then 56 to make 56 on top of the Stack

--- StackAndQueueDataStructure/Program.cs
+++ StackAndQueueDataStructure/Program.cs
@@ -10,6 +10,14 @@ namespace StackAndQueueDataStructure
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("implementation of Stack and queue datastructure using linked list");
+            Console.WriteLine("Stack operations\n1.Push");
+            StackOperations s = new StackOperations();
+            s.push(70);
+            s.push(30);
+            s.push(56);
+            s.display();
+            Console.ReadLine();
         }
     }
 }
---
+++ StackAndQueueDataStructure/StackOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueDataStructure
+{
+    internal class StackOperations
+    {
+       public Node top;
+        public void push(int data)
+        {
+            Node item = new Node(data);
+            if (top == null)
+            {
+                top=item;
+                Console.WriteLine("{0} is pushed into the stack",item.data);
+            }
+            else
+            {
+               item.next=top;
+                top=item;
+                Console.WriteLine("{0} is pushed into the stack", item.data);
+            }
+        }
+        public void display()
+        {
+            Node temp = top;
+            if(temp == null)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data+" ");
+                    temp=temp.next;
+                }
+            }
+        }
+    }
+}
